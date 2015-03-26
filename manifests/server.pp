e611f0da1bf6341426632413579a54dd0e5bdbd3
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-03-26 18:16:57 +0100

    Update server.pp

--- manifests/server.pp
+++ manifests/server.pp
@@ -36,8 +36,8 @@ class burp::server {
   
   # Modify backup_stats file so that logstash can use it as input
   if $burp::backup_stats_logstash == true {
-    file { '/etc/burp/server_script_post':
-      content => template("burp/server_script_post.erb"),
+    file { '/etc/burp/notify_script':
+      content => template("burp/notify_script.erb"),
       mode    => 0700,
       require => Package['burp']
     }
