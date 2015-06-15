22a24964884ceecd7d60ced3ff92768e3ce452ad
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-06-15 15:59:34 +0200

    Install from source

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,30 +2,38 @@
 #
 class burp::package {
 
+  $burp_requires = [ "librsync-dev", "libz-dev", "libz-dev", "libssl-dev", "uthash-dev", "libyajl-dev" ]
+
   if $::operatingsystem != 'Ubuntu' {
     notice('Operatingsystem not supported, perform manual burp installation.')
   }
+  
+  else {
+  
+    package { $burp_requires:
+      ensure => installed
+    }
 
-  if $::operatingsystem == 'Ubuntu' {
+    wget::fetch { "burp_package":
+      source      => "http://burp.grke.org/downloads/burp-${burp::version}/burp-${burp::version}.tar.bz2",
+      destination => "/tmp/burp-${burp::version}.tar.bz2",
+      timeout     => 0,
+      verbose     => false,
+    }
 
-    if !defined(Class['apt']) {
-      class { 'apt': }
+    exec { 'unpack_burp':
+      command => "/bin/tar -xzvf /tmp/burp-${burp::version}.tar.bz2 -C /tmp/",
+      unless  => "/usr/bin/test -d /tmp/burp-${burp::version}",
+      require => Wget::fetch['burp_package']
     }
     
-    apt::ppa { 'ppa:hugo-vanduijn/burp-latest':
-      require => File['/etc/apt/sources.list.d']
+    exec { 'configure_burp':
+      command => '/tmp/burp-${burp::version}/configure.sh && /usr/bin/make && /usr/bin/make install',
+      cwd     => '/tmp/burp-${burp::version}',
+      unless  => '/usr/bin/test -d /etc/burp',
+      require => Exec['unpack_burp']
     }
-
-    apt::key {'ppa:hugo-vanduijn/burp-latest':
-      key        => 'A4EF7A24',
-      key_server => 'keyserver.ubuntu.com',
-      require    => File['/etc/apt/sources.list.d']
-    }
-
-    package { 'burp':
-      ensure  => $burp::version,
-      require => [Apt::Ppa['ppa:hugo-vanduijn/burp-latest'], Apt::Key['ppa:hugo-vanduijn/burp-latest']]
-    }
+    
   }
 
 }
