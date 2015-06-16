2c8f657596a1bcf45d27b96596596962cc41fdd4
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-06-16 09:57:50 +0200

    Update server.pp

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,37 +2,13 @@
 #
 class burp::server {
 
-  file { '/etc/burp/clientconfdir':
-    ensure  => 'directory',
-    require => Package['burp']
-  }
-
-  file { '/etc/burp/burp-server.conf':
-    ensure  => present,
-    mode    => '600',
-    content => template("burp/burp-server.conf.erb"),
-    require => Package['burp']
-  }
-
-  file { '/etc/default/burp':
-    ensure  => present,
-    mode    => '600',
-    content => template("burp/default.erb"),
-    require => Package['burp']
-  }
-
+  # Common settings for all clients
   file { '/etc/burp/clientconfdir/incexc/common':
     ensure  => present,
     mode    => '600',
     content => template("burp/common.erb"),
-    require => File['/etc/burp/clientconfdir']
   }
 
-  service { 'burp':
-    ensure  => 'running',
-    require => File['/etc/burp/burp-server.conf'] 
-  }
-  
   # Modify backup_stats file so that logstash can use it as input
   #if $burp::backup_stats_logstash == true {
   #  file { '/etc/burp/notify_script':
