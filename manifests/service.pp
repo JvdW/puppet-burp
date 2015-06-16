6a0d680ed85f36e3bcc2458b86a7cc579804e60c
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-06-16 09:59:03 +0200

    Create service.pp

---
+++ manifests/service.pp
@@ -0,0 +1,16 @@
+
+
+
+
+# Service file
+  file { '/etc/default/burp':
+    ensure  => present,
+    mode    => '600',
+    content => template("burp/default.erb"),
+  }
+
+  # Start service
+  service { 'burp':
+    ensure  => 'running',
+    require => File['/etc/default/burp'] 
+  }
