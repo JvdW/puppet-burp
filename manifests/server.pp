c7f76cd2218d19e19bf8af82ed1a11a4c1fb0147
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-06-16 14:36:28 +0200

    Update server.pp

--- manifests/server.pp
+++ manifests/server.pp
@@ -13,6 +13,6 @@ class burp::server {
   create_resources( 'burp::defines::clientconf', $burp::clientconf_hash )
   
   # Set settings in /etc/burp/burp-server.conf
-  create_resources( 'burp::defines::burp-server', $burp::burp_server_hash )
+  create_resources( 'burp::defines::burp_server', $burp::burp_server_hash )
 
 }
