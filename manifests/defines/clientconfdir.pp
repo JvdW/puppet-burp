a935736e372b4f584b1942779631684d1a176da9
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-06-19 10:50:45 +0200

    Update clientconfdir.pp

--- manifests/defines/clientconfdir.pp
+++ manifests/defines/clientconfdir.pp
@@ -1,11 +1,14 @@
 # Define: burp::defines::clientconfdir
 #
 define burp::defines::clientconfdir (
-  $password,
+  $clientconfdir_hash,
   ) {
 
   file { "/etc/burp/clientconfdir/${title}":
     mode => "600",
   }
+  
+  # Fill /etc/burp/clientconfdir directory, each client needs a config file
+  create_resources( 'burp::defines::clientconf', $clientconf_hash )
 
 }
