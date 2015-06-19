58f78449f49e5cc2d6cca2db0727a6368af5fc1a
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-06-19 09:38:30 +0200

    Update and rename clientconf.pp to clientconfdir.pp

---
+++ manifests/defines/clientconfdir.pp
@@ -0,0 +1,11 @@
+# Define: burp::defines::clientconfdir
+#
+define burp::defines::clientconfdir (
+  $password,
+  ) {
+
+  file { "/etc/burp/clientconfdir/${title}":
+    mode    => "600",
+  }
+
+}
