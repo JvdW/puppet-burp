7728f58187103d89ebbe9241d2fcf6a59515de32
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-07-09 20:21:49 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # == Class: burp
 #
-# Full description of class burp here.
+# Install and manage Burp: a network backup and restore program.
 #
 # === Parameters
 #
@@ -27,17 +27,6 @@
 # [*burp_hash*]
 #   Burp client settings.
 #
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
 # === Examples
 #
 #  class { burp:
@@ -52,7 +41,7 @@
 #
 # === Copyright
 #
-# Copyright 2014 Your name here, unless otherwise noted.
+# Copyright 2015 Rudi Broekhuizen.
 #
 class burp (
 
