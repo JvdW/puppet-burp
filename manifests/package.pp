32e9c289afe508b8a177be02cd091c6af6a0d459
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-07-09 15:40:47 +0200

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,7 +2,7 @@
 #
 class burp::package {
 
-  $burp_requires = [ "make", "build-essential", "g++", "librsync-dev", "libz-dev", "libssl-dev", "uthash-dev", "libyajl-dev" ]
+  $burp_requires = [ "make", "build-essential", "g++", "libacl1-dev", "librsync-dev", "libz-dev", "libssl-dev", "uthash-dev", "libyajl-dev" ]
 
   if $::operatingsystem != 'Ubuntu' {
     notice('Operatingsystem not supported, perform manual burp installation.')
