3358d9fd72442d36358beec3556e1e2fff38d450
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-07-13 13:58:40 +0200

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -9,10 +9,17 @@ class burp::service {
   #  content => template("burp/default.erb"),
   #}
 
+# cp /tmp/burp-1.4.36/debian/burp.default /etc/default/burp
+# cp /tmp/burp-1.4.36/debian/init /etc/init.d/burp
+# chmod +x /etc/init.d/burp
+# vi /etc/default/burp run=yes
+
+
   # Start service
   service { 'burp':
     ensure  => 'running',
     #require => File['/etc/default/burp'] 
   }
+
 
 }
