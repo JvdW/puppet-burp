c4e93ac58f9cf1b90923362bd2acd7b11071ffb4
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-07-14 09:02:25 +0200

    Update client.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -5,4 +5,13 @@ class burp::client {
   # Set settings in /etc/burp/burp.conf
   create_resources( 'burp::defines::burp', $burp::burp_hash )
 
+
+  # Set cname as fqdn
+  ini_setting { "/etc/burp/burp.conf":
+    ensure  => present,
+    path    => "/etc/burp/burp.conf",
+    setting => cname,
+    value   => $fqdn,
+  }
+
 }
