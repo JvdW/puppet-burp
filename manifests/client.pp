d8961beef9860152c11c7970758111eb93c8f6fa
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-11-10 23:02:38 +0100

    Update client.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -4,7 +4,7 @@ class burp::client {
 
   $burp_hash_defaults = { 'path'           => '/etc/burp/burp.conf',
                           'section_prefix' => '#[',
-	                      }
+	                }
 
   # Set settings in /etc/burp/burp.conf
   create_ini_settings($burp::burp_hash, $burp_hash_defaults)
